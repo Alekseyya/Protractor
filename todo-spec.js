90cd54d32b2276ff6bde4d2ab288f12f774091c5
Aleksey Yarchuk <alekseyyarchuk@gmail.com>
2018-01-26 15:45:52 +0300

    Selenium tasks

--- todo-spec.js
+++ todo-spec.js
@@ -1,17 +1,19 @@
-describe('angularjs homepage todo list', function() {
-    it('should add a todo', function() {
-      browser.get('https://angularjs.org');
-  
-      // element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-      // element(by.css('[value="add"]')).click();
-  
-      // var todoList = element.all(by.repeater('todo in todoList.todos'));
-      // expect(todoList.count()).toEqual(3);
-      // expect(todoList.get(2).getText()).toEqual('write first protractor test');
-  
-      // // You wrote your first test, cross it off the list
-      // todoList.get(2).element(by.css('input')).click();
-      // var completedAmount = element.all(by.css('.done-true'));
-      // expect(completedAmount.count()).toEqual(2);
+describe('Я тестирую баклажаны', function() {
+    it('я бросаю их на землю', function() {
+      browser.get('http://www.autopiter.ru');
+
+    //element(by.xpath('//input[@id="SearchNumber"]')).sendKeys('write first protractor test');
+      let findElement = driver.findElement(By.xpath('//input[@id="SearchNumber"]'));
+      findElement.sendKeys("asdasd");
+    
+    // var todoList = element.all(by.repeater('todo in todoList.todos'));
+    // expect(todoList.count()).toEqual(3);
+    // expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // // You wrote your first test, cross it off the list
+    // todoList.get(2).element(by.css('input')).click();
+    // var completedAmount = element.all(by.css('.done-true'));
+    // expect(completedAmount.count()).toEqual(2);
+      
     });
   });
