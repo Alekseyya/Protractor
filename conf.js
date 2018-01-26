2e5029fd52ddde6aecd9d402792b5dedfadbb43b
Aleksey Yarchuk <alekseyyarchuk@gmail.com>
2018-01-26 19:10:11 +0300

    Addd new tests

--- conf.js
+++ conf.js
@@ -1,4 +1,28 @@
 exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['todo-spec.js']
+  'autoStartStopServer': true,
+
+  capabilities: {
+    'browserName': 'internet explorer'
+  },
+ 
+  //directConnect: true,  
+  // multiCapabilities: [{
+  //   'browserName': 'firefox'
+  // }, {
+  //   'browserName': 'chrome'
+  // }, {
+  //   'browserName': 'internet explorer'
+  // }],
+  
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['my-spec.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
 };
---
+++ my-spec.js
@@ -0,0 +1,12 @@
+describe("Tests several browsers", function () {
+    it("Test firefox", function(){
+        browser.ignoreSynchronization = true;
+        browser.get("http://www.bbc.com/");        
+        element(by.css('#orb-search-q')).sendKeys("Hello");
+        browser.sleep(5000);
+        browser.manage().window().maximize();
+        element(by.css("#orb-nav-links > ul > li:nth-child(1)")).click();
+        browser.sleep(5000);
+        //element(by.css('#orb-search-q')).sendKeys('Julie');
+    });    
+});
