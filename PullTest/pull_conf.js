d35c34f8eb40da88a0bf25d3668f2e8efa65f01c
Aleksey Yarchuk <alekseyyarchuk@gmail.com>
2018-02-07 19:34:52 +0300

    aaaa

--- PullTest/pull_conf.js
+++ PullTest/pull_conf.js
@@ -18,26 +18,26 @@ exports.config = {
   jasmineNodeOpts: {
     defaultTimeoutInterval: 30000
   },
-  onPrepare: function () {
-    jasmine.getEnv().addReporter(
-      new Jasmine2HtmlReporter({
-        savePath: './Reports',
-        fileNamePrefix: 'Report',
-        fileNameDateSuffix: true
-      })
-    );
-  },
-  framework: 'jasmine2',
-  onPrepare: function () {
-    var AllureReporter = require('jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter());
-    jasmine.getEnv().afterEach(function(done){
-      browser.takeScreenshot().then(function (png) {
-        allure.createAttachment('Screenshot', function () {
-          return new Buffer(png, 'base64')
-        }, 'image/png')();
-        done();
-      })
-    });
-  }
+  // onPrepare: function () {
+  //   jasmine.getEnv().addReporter(
+  //     new Jasmine2HtmlReporter({
+  //       savePath: './Reports',
+  //       fileNamePrefix: 'Report',
+  //       fileNameDateSuffix: true
+  //     })
+  //   );
+  // },
+  // framework: 'jasmine2',
+  // onPrepare: function () {
+  //   var AllureReporter = require('jasmine-allure-reporter');
+  //   jasmine.getEnv().addReporter(new AllureReporter());
+  //   jasmine.getEnv().afterEach(function(done){
+  //     browser.takeScreenshot().then(function (png) {
+  //       allure.createAttachment('Screenshot', function () {
+  //         return new Buffer(png, 'base64')
+  //       }, 'image/png')();
+  //       done();
+  //     })
+  //   });
+  // }
 };
