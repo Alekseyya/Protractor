e35be33402410d32239aa9913750c7b4424b8468
Aleksey Yarchuk <alekseyyarchuk@gmail.com>
2018-02-07 17:03:33 +0300

    Addd

--- singleton-spec.js
+++ singleton-spec.js
@@ -1,12 +1,12 @@
 describe("Tests several browsers", function () {
     browser.ignoreSynchronization = true;
     it("Test firefox", function(){        
-        browser.get("http://www.bbc.com/");        
-        element(by.css('#orb-search-q')).sendKeys("Hello");
-        browser.sleep(5000);
-        browser.manage().window().maximize();
-        element(by.css("#orb-nav-links > ul > li:nth-child(1)")).click();
-        browser.sleep(5000);
+        browser.get("http://www.yandex.com/");        
+        // element(by.css('#orb-search-q')).sendKeys("Hello");
+        // browser.sleep(5000);
+        // browser.manage().window().maximize();
+        // element(by.css("#orb-nav-links > ul > li:nth-child(1)")).click();
+        // browser.sleep(5000);
         //element(by.css('#orb-search-q')).sendKeys('Julie');
     });
     // it("Test firefox", function(){
