c213c7e5089619071e163adc10541300ea2a447f
Aleksey Yarchuk <alekseyyarchuk@gmail.com>
2018-02-07 19:44:07 +0300

    bbb

--- PullTest/pull_conf.js
+++ PullTest/pull_conf.js
@@ -27,17 +27,17 @@ exports.config = {
   //     })
   //   );
   // },
-  // framework: 'jasmine2',
-  // onPrepare: function () {
-  //   var AllureReporter = require('jasmine-allure-reporter');
-  //   jasmine.getEnv().addReporter(new AllureReporter());
-  //   jasmine.getEnv().afterEach(function(done){
-  //     browser.takeScreenshot().then(function (png) {
-  //       allure.createAttachment('Screenshot', function () {
-  //         return new Buffer(png, 'base64')
-  //       }, 'image/png')();
-  //       done();
-  //     })
-  //   });
-  // }
+  framework: 'jasmine2',
+  onPrepare: function () {
+    var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
+  }
 };
